8b35311ce776c7ba86477f009753f0bb13e46212
Erik Mikac <mikacew@gmail.com>
2021-03-15 18:53:55 -0400

    Initial Commit

---
+++ Shared/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Shared
+//
+//  Created by Erik Mikac on 3/15/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
